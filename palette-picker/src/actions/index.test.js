fe6f9dcab7efe0ffc8ac474e38d4a7469407e1f4
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-11 21:17:33 -0600

    Test actions
    Co-authored-by: Kim Myers <kimberly.achkio@gmail.com>

---
+++ palette-picker/src/actions/index.test.js
@@ -0,0 +1,39 @@
+import * as actions from './index';
+
+describe('actions', () => {
+
+  it('should return a type of HAS_ERROR with a message', () => {
+    const message = 'Something went wrong'
+    const expected = {
+      type: 'HAS_ERROR',
+      message
+    }
+
+    const result = actions.hasError(message)
+
+    expect(result).toEqual(expected)
+  })
+
+  it('should return a type of STORE_PROJECTS with an array of projects', () => {
+    const mockProjects = [
+      { project_id: 1,
+        project_name: "Travel App",
+        created_at: "2019-05-10T17:45:09.612Z",
+        updated_at: "2019-05-10T17:45:09.612Z"
+      },
+      { project_id: 2,
+        project_name: "Fitness App",
+        created_at: "2019-05-10T17:45:09.634Z",
+        updated_at: "2019-05-10T17:45:09.634Z"
+      }
+    ]
+    const expected = {
+      type: 'STORE_PROJECTS',
+      projects: mockProjects
+    }
+
+    const result = actions.storeProjects(mockProjects)
+
+    expect(result).toEqual(expected)
+  })
+})
