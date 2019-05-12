8a8627feb3a2fafda9591c7544f7e7e7489dd5d7
“Kimberly <kimberly.achkio@gmail.com>
2019-05-12 12:15:01 -0600

    Write method to randomly generate 5 colors and update in state
    Co-Authored-By: Jessica Hansen <jessicalynhansen@gmail.com>

--- palette-picker/src/containers/App/App.js
+++ palette-picker/src/containers/App/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchProjects } from '../../thunks/fetchProjects';
+import { storeColors } from '../../actions';
 
 export class App extends Component {
 
   componentDidMount() {
     const url = `https://palette-picker-mfjk.herokuapp.com/api/v1/projects`
     this.props.fetchProjects(url)
+    this.generateRandomColor()
+  }
+
+  generateRandomColor = () => {
+    let randomColors = []
+    for (let i = 0; i < 5; i++ ) {
+      const randomColor = Math.floor(Math.random()*16777215).toString(16)
+      randomColors.push(`#${randomColor}`)
+    }
+    this.props.storeColors(randomColors)
   }
 
   render() {
@@ -17,16 +28,11 @@ export class App extends Component {
           <p>Here we are</p>
         </header>
         <div className="colors-wrapper">
-          <div className="palette-color">
-          </div>
-          <div className="palette-color">
-          </div>
-          <div className="palette-color">
-          </div>
-          <div className="palette-color">
-          </div>
-          <div className="palette-color">
-          </div>
+          <div className="palette-color"></div>
+          <div className="palette-color"></div>
+          <div className="palette-color"></div>
+          <div className="palette-color"></div>
+          <div className="palette-color"></div>
         </div>
       </div>
     );
@@ -35,11 +41,13 @@ export class App extends Component {
 
 export const mapStateToProps = (state) => ({
   error: state.error,
-  projects: state.projects
+  projects: state.projects,
+  currentColors: state.currentColors
 })
 
 export const mapDispatchToProps = (dispatch) => ({
-  fetchProjects: (url) => dispatch(fetchProjects(url))
+  fetchProjects: (url) => dispatch(fetchProjects(url)),
+  storeColors: (colors) => dispatch(storeColors(colors))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
