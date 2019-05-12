718434e1f3270de42dbff53437526edee86bf688
“Kimberly <kimberly.achkio@gmail.com>
2019-05-12 11:49:21 -0600

    Add currentColors array to state
    Co-Authored-By: Jessica Hansen <jessicalynhansen@gmail.com>

--- palette-picker/src/reducers/index.js
+++ palette-picker/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { errorReducer } from './errorReducer';
 import { projectsReducer } from './projectsReducer';
+import { colorsReducer } from './colorsReducer';
 
 export const rootReducer = combineReducers({
   error: errorReducer,
-  projects: projectsReducer
+  projects: projectsReducer,
+  currentColors: colorsReducer
 });
