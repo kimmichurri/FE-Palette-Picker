741382e94c4225b97f08d0ec1cf3cb63972be566
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-13 14:33:21 -0600

    Create generate new palette button
    Co-authored-by: Kim Myers <kimberly.achkio@gmail.com>

--- palette-picker/src/containers/App/App.js
+++ palette-picker/src/containers/App/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchProjects } from '../../thunks/fetchProjects';
 import { storeColors } from '../../actions';
+import closeButton from '../../assets/closeButton.png';
+import emptyStar from '../../assets/emptyStar.png';
+import filledStar from '../../assets/filledStar.png';
+import generate from '../../assets/generate.png';
 
 export class App extends Component {
 
@@ -26,8 +30,13 @@ export class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <p>Here we are</p>
+          <h1>Palette Picker</h1>
         </header>
+        <h2>Generate New Palette
+          <button>
+            <img src={generate} alt={'Generate New Palette icon'}/>
+          </button>
+        </h2>
         <div className="colors-wrapper">
           <div className="palette-color" style={{ backgroundColor: currentColors[0] }}></div>
           <div className="palette-color" style={{ backgroundColor: currentColors[1] }}></div>
