8af3877535831c0ed4ba5491431f27aed49b9bd0
“Kimberly <kimberly.achkio@gmail.com>
2019-05-14 16:23:07 -0600

    Use dropdown to update state for project_id
    Co-Authored-By: Jessica Hansen <jessicalynhansen@gmail.com>

--- palette-picker/src/containers/Palette/Palette.js
+++ palette-picker/src/containers/Palette/Palette.js
@@ -11,7 +11,7 @@ export class Palette extends Component {
     super();
     this.state = {
       palette_name: '',
-      project_id: 1,
+      project_id: 0,
       color_1: '',
       color_2: '',
       color_3: '',
@@ -44,11 +44,6 @@ export class Palette extends Component {
     this.props.storeColors(updatedColors)
   }
 
-  updatePaletteName = (e) => {
-    const { name, value } = e.target
-    this.setState({ [name]: value })
-  }
-
   setPalette = async (e) => {
     e.preventDefault()
     await this.props.currentColors.forEach((color, index) => {
@@ -64,9 +59,18 @@ export class Palette extends Component {
     await this.props.postNewPalette(options, body)
   }
 
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState({ [name]: value })
+  }
+
   render() {
     const colorSwatches = this.props.currentColors.map((color, index) => {
       return <Color key={color.color} {...color} index={index} handleToggle={this.handleToggle} />
+    })
+    const projectList = this.props.projects.map((project, index) => {
+      let projectName = project.project_name
+      return <option key={`option-${projectName}-${index}`} value={project.project_id} >{projectName}</option>
     })
     return(
       <div className="colors-wrapper">
@@ -75,8 +79,11 @@ export class Palette extends Component {
             <img src={generate} alt={'Generate New Palette icon'} />
           </button>
         </h2>
+        <select value={this.state.project_id} name="project_id" onChange={this.handleChange}>
+          {projectList}
+        </select>
         <form onSubmit={this.setPalette}>
-          <input type="text" onChange={this.updatePaletteName} value={this.state.palette_name} name="palette_name" />
+          <input type="text" onChange={this.handleChange} value={this.state.palette_name} name="palette_name" />
           <button className="palette-button">Save Palette to Project</button>
         </form>
         {colorSwatches}
@@ -86,7 +93,8 @@ export class Palette extends Component {
 }
 
 export const mapStateToProps = (state) => ({
-  currentColors: state.currentColors
+  currentColors: state.currentColors,
+  projects: state.projects
 })
 
 export const mapDispatchToProps = (dispatch) => ({
