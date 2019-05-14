839a7115915f0c251310429eb537ffc37b069632
“Kimberly <kimberly.achkio@gmail.com>
2019-05-13 21:07:38 -0600

    Update tests for projectsReducer

--- palette-picker/src/reducers/projectsReducer.test.js
+++ palette-picker/src/reducers/projectsReducer.test.js
@@ -2,7 +2,7 @@ import { projectsReducer } from './projectsReducer';
 import * as actions from '../actions';
 
 describe('projectsReducer', () => {
-  const mockState = []
+  let mockState = []
 
   it('should return default state', () => {
     const action = {}
@@ -30,5 +30,37 @@ describe('projectsReducer', () => {
     const result = projectsReducer(mockState, action)
     
     expect(result).toEqual(mockProjects)
+  })
+
+  it('should return an updated array of projects when type is ADD_PROJECT', () => {
+    mockState = [
+      { project_id: 1,
+        project_name: "Travel App",
+      },
+      { project_id: 2,
+        project_name: "Fitness App",
+      }
+    ]
+    const mockNewProject = { 
+      project_id: 3,
+      project_name: "Bird Watcher App",
+    }
+    const expected = [
+      { project_id: 1,
+        project_name: "Travel App",
+      },
+      { project_id: 2,
+        project_name: "Fitness App",
+      },
+      { project_id: 3,
+        project_name: "Bird Watcher App",
+      }
+    ]
+
+    const action = actions.addProject(mockNewProject)
+
+    const result = projectsReducer(mockState, action)
+
+    expect(result).toEqual(expected)
   })
 })
