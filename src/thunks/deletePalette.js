ccd4a4bdab6b9d1f3c3f7dc91c5fda2f5d90bd9e
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-15 15:16:24 -0600

    Set environment variables
    Co-authored-by: Kim Myers <kimberly.achkio@gmail.com>

--- src/thunks/deletePalette.js
+++ src/thunks/deletePalette.js
@@ -3,7 +3,8 @@ import { fetchOptionsCreator } from '../utils/fetchOptionsCreator';
 
 export const deletePalette = (id) => {
   return async (dispatch) => {
-    const url = `https://palette-picker-mfjk.herokuapp.com/api/v1/palettes/${id}`
+    const baseUrl = process.env.REACT_APP_BACKEND_URL
+    const url = `${baseUrl}/api/v1/palettes/${id}`
     try {
       const options = fetchOptionsCreator('DELETE')
       const response = await fetch(url, options)
