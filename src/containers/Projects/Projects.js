4817841beb9323e88729dc1aae48674bfc565128
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-15 17:31:55 -0600

    Style individual project and palette boxes

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -45,7 +45,7 @@ export class Projects extends Component {
 
   render() {
     const projectsToDisplay = this.props.projects.map((project, index) => {
-      return <div key={`${project.project_name}-${index}`}>
+      return <div className="individual-projects" key={`${project.project_name}-${index}`}>
         <h3>{project.project_name}</h3>
           <button onClick={this.deleteProject}>
             <img src={closeButton} alt={'Delete Project icon'} id={project.project_id}/>
@@ -53,10 +53,10 @@ export class Projects extends Component {
         {
           this.props.palettes.map((palette, index) => {
             if (palette.project_id === project.project_id) {
-              return <div key={`${palette.palette_name}-${index}`}>
-                <h4>{palette.palette_name}</h4>
+              return <div className="project-palettes" key={`${palette.palette_name}-${index}`}>
+                <h5>{palette.palette_name}</h5>
                 <button onClick={this.deletePalette}>
-                  <img src={closeButton} alt={'Delete Palette icon'} id={palette.palette_id}/>
+                  <img className="delete-palette-button" src={closeButton} alt={'Delete Palette icon'} id={palette.palette_id}/>
                 </button>
               </div>
             }
@@ -70,7 +70,9 @@ export class Projects extends Component {
         <input type="text" onChange={this.updateProjectName} value={this.state.project_name} name="project_name" />
         <button className="project-button">Add New Project</button>
       </form>
-      {projectsToDisplay}
+      <div className="projects-to-display">
+        {projectsToDisplay}
+      </div>
       </div>
     )
   }
