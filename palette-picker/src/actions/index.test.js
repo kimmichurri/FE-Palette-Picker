ead50c1aa9eee68081bdc3d9f8d7b9c07b245e75
“Kimberly <kimberly.achkio@gmail.com>
2019-05-14 19:32:28 -0600

    Fully update actions test

--- palette-picker/src/actions/index.test.js
+++ palette-picker/src/actions/index.test.js
@@ -85,7 +85,35 @@ describe('actions', () => {
   })
 
   it('should return a type of STORE_PALETTES with an array of palettes', () => {
-    
+    const mockPalettes = [
+    {
+        palette_name: "one",
+        palette_id: 68,
+        project_id: 1,
+        color_1: "#605976",
+        color_2: "#3241f2",
+        color_3: "#dc677",
+        color_4: "#1c1eb5",
+        color_5: "#c54e8b"
+    },
+    {
+      palette_name: "two",
+      palette_id: 68,
+      project_id: 1,
+      color_1: "#c54e8b",
+      color_2: "#3241f2",
+      color_3: "#dc677",
+      color_4: "#1c1eb5",
+      color_5: "#605976"
+  }]
+  const expected = {
+    type: 'STORE_PALETTES',
+    palettes: mockPalettes
+  }
+
+  const result = actions.storePalettes(mockPalettes)
+
+  expect(result).toEqual(expected)
   })
 
 });
