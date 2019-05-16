72d2210940fab8f984c1fd01898d3dd9cec7c52b
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-15 23:50:23 -0600

    Move save palette to projects container

--- src/thunks/postNewPalette.js
+++ src/thunks/postNewPalette.js
@@ -5,6 +5,7 @@ export const postNewPalette = (options, body) => {
     const url = `${process.env.REACT_APP_BACKEND_URL}/api/v1/palettes`
     try {
       const response = await fetch(url, options)
+      console.log("got here", response.ok)
       if (!response.ok) {
         throw Error (response.statusText)
       }
